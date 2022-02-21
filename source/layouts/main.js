5763df531e48c56e55995fa221c9a8b6bd5d50d5
Andrey Efimov <efiand@ya.ru>
2022-02-21 13:00:22 +0300

    Контент главной страницы

--- source/layouts/main.js
+++ source/layouts/main.js
@@ -1,6 +1,82 @@
-() => ({
-	heading: 'Страница в разработке!',
-	lang: 'ru',
-	projectName: 'My project',
-	title: 'Страница в разработке'
-});
+(_data, Util) => {
+	Util.addBlankLinks = (item) => ({
+		...item,
+		href: 'blank.html'
+	});
+
+	const phoneNumber = '+7 (812) 812-12-12';
+
+	return {
+		address: 'Санкт-Петербург, набережная реки Карповки, 5, литера П.',
+		heading: 'Страница в разработке!',
+		lang: 'ru',
+		mainMenu: {
+			list: [
+				{
+					list: [
+						{
+							title: 'Виртуальная реальность'
+						},
+						{
+							title: 'Моноподы для cелфи'
+						},
+						{
+							title: 'Экшн-камеры'
+						},
+						{
+							title: 'Фитнес-браслеты'
+						},
+						{
+							title: 'Умные часы'
+						},
+						{
+							title: 'Квадрокоптеры'
+						}
+					].map(Util.addBlankLinks),
+					title: 'Каталог товаров'
+				},
+				{
+					title: 'Доставка'
+				},
+				{
+					title: 'Гарантия'
+				},
+				{
+					title: 'Контакты'
+				}
+			].map(Util.addBlankLinks)
+		},
+		projectName: 'Device',
+		searchForm: {
+			action: 'https://echo.htmlacademy.ru',
+			fields: [
+				{
+					label: 'Поиск',
+					name: 'search',
+					placeholder: 'Текст поиска'
+				}
+			],
+			submitText: 'Найти'
+		},
+		tel: {
+			formattedValue: phoneNumber,
+			shortTitle: 'Тел.',
+			value: Util.numberize(phoneNumber)
+		},
+		title: 'Страница в разработке',
+		userMenu: {
+			list: [
+				{
+					title: 'Войти'
+				},
+				{
+					title: 'Сравнить'
+				},
+				{
+					label: '2',
+					title: 'Корзина'
+				}
+			].map(Util.addBlankLinks)
+		}
+	};
+};
--- source/layouts/pages/index.js
+++ source/layouts/pages/index.js
@@ -1,4 +1,143 @@
-() => ({
-	heading: 'Hello world!',
-	title: 'Главная'
-});
+(data) => {
+	const { addBlankLinks } = data;
+
+	return {
+		accentLink: {
+			href: 'blank.html',
+			title: 'Доставим редкий товар под заказ'
+		},
+		contactsInfo: {
+			content: `
+				<p>Вы можете забрать товар сами, заехав в наш офис. Заодно сможете проверить работоспособность покупки.</p>
+				<h3>Интернет-магазин «Девайс»</h3>
+				<p>
+					${data.address}<br>
+					<a href="tel:${data.tel.value}">${data.tel.formattedValue}</a>
+				</p>
+				<h3>Время работы</h3>
+				<p>
+					пн—пт: с 10:00 до 22:00<br>
+					сб—вс: с 10:00 до 19:00
+				</p>
+			`,
+			footLink: {
+				href: 'blank.html',
+				title: 'Напишите нам'
+			},
+			heading: 'Контакты'
+		},
+		deliveryInfo: {
+			content: `
+				<p>Огромный выбор гаджетов не оставит равнодушным гика, который есть в каждом из нас.</p>
+				<p>Мы можем доставить ваш товар в самые отдалённые уголки России транспортными компаниями:</p>
+				<ul>
+					<li>Меловые линии</li>
+					<li>СДИК</li>
+					<li>ТоксБерри</li>
+				</ul>
+			`,
+			footLink: {
+				href: 'blank.html',
+				title: 'Подробнее о нас'
+			},
+			heading: 'О нас'
+		},
+		heading: 'Device',
+		promo: {
+			labels: {
+				dateilsLink: 'Подробнее'
+			},
+			list: [
+				{
+					content: '<p>Этот обычный, на первый взгляд, квадрокоптер оснащен мощным лазером, замаскированным под стандартную камеру.</p>',
+					heading: '',
+					lead: 'Порхает как бабочка, жалит как пчела!',
+					params: [
+						{
+							key: 'Дальность полета',
+							value: '800 м'
+						},
+						{
+							key: 'Радиус поражения',
+							value: '50 м'
+						}
+					]
+				},
+				{
+					content: '<p></p>',
+					heading: '',
+					lead: '',
+					params: [
+						{
+							key: '',
+							value: ''
+						},
+						{
+							key: '',
+							value: ''
+						}
+					]
+				},
+				{
+					content: '',
+					heading: '',
+					lead: '',
+					params: [
+						{
+							key: '',
+							value: ''
+						},
+						{
+							key: '',
+							value: ''
+						}
+					]
+				}
+			]
+		},
+		services: {
+			list: [
+				{
+					content: 'Мы с удовольствием доставим ваш товар прямо к вашему подъезду совершенно бесплатно! Ведь мы неплохо заработаем, поднимая его на ваш этаж.',
+					title: 'Доставка'
+				},
+				{
+					content: '',
+					title: 'Гарантия'
+				},
+				{
+					content: '',
+					title: 'Кредит'
+				}
+			],
+			tabsList: [
+				{
+					title: 'Доставка'
+				},
+				{
+					title: 'Гарантия'
+				},
+				{
+					title: 'Кредит'
+				}
+			].map(addBlankLinks)
+		},
+		subscribeInfo: {
+			content: 'Еженедельный дайджест с обзорами новинок, длительными тест-драйвами, распаковками, а также акции, спецпредложения и вот это вот всё.',
+			form: {
+				action: 'https://echo.htmlacademy.ru',
+				fields: [
+					{
+						label: 'E-mail',
+						name: 'email',
+						placeholder: 'Ваш e-mail'
+					}
+				],
+				submitText: 'Подписаться'
+			},
+			heading: 'Подпишитесь на рассылку',
+			infoTag: 'Это бесплатно'
+		},
+		title: 'Главная'
+	};
+};
