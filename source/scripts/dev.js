d618b296207368e19d84e89a77d736df79fdc2e2
Andrey Efimov <efiand@ya.ru>
2022-03-15 15:28:02 +0300

    Пиксельперфект по скриншотам академии

--- source/scripts/dev.js
+++ source/scripts/dev.js
@@ -1,11 +1,10 @@
 import loadPP from 'pineglade-pp';
 
 const BREAKPOINT = 1240;
-const BREAKPOINT_MAX = 1300;
 
 window.pinegladePP = {
-	breakpoints: [BREAKPOINT, BREAKPOINT_MAX],
-	folder: 'pixelperfect'
+	breakpoints: [BREAKPOINT],
+	ext: 'png'
 };
 
 loadPP();
